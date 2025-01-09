42b1e5541517ad59e27caa493099d7effe3722a9
siddharth03-del <siddharthsingh9361@gmail.com>
2025-01-09 15:51:28 +0530

    Added chatting feature

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axios_instance = axios.create({
-    baseURL : "https://imagegram03.onrender.com/api/v1"
+    baseURL : "https://imagegram03.onrender.com/ping"
 })
 export default axios_instance;
